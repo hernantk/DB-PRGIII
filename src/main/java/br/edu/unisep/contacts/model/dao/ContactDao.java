c6f4a409b21f2c393f80c20e870c05e281b9a52f
hernan <Hernantkup@gmail.com>
2020-03-25 20:09:18 -0300

    Initial commit

---
+++ src/main/java/br/edu/unisep/contacts/model/dao/ContactDao.java
@@ -0,0 +1,15 @@
+package br.edu.unisep.contacts.model.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ContactDao {
+    private Connection connect() throws  ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+
+        var connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/contatos",
+                "postgres","jdfnil54");
+    return connection;
+    }
+}
---
+++ src/main/java/br/edu/unisep/contacts/model/entity/Contact.java
@@ -0,0 +1,14 @@
+package br.edu.unisep.contacts.model.entity;
+
+
+import lombok.Data;
+
+
+
+@Data
+public class Contact {
+    private Integer id;
+    private  String nome;
+    private String email;
+
+}
