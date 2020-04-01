495aa830511aeb6cd545dad553f7fab84c7ea2c6
hernan <Hernantkup@gmail.com>
2020-03-31 22:22:40 -0300

    Aula 31/03

--- src/main/java/br/edu/unisep/contacts/beans/ListContactsBean.java
+++ src/main/java/br/edu/unisep/contacts/beans/ListContactsBean.java
@@ -2,6 +2,7 @@ package br.edu.unisep.contacts.beans;
 
 
 import br.edu.unisep.contacts.model.entity.Contact;
+import br.edu.unisep.contacts.usecase.DeleteContactUseCase;
 import br.edu.unisep.contacts.usecase.ListContactsUseCase;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,14 +16,22 @@ import java.util.List;
 @Named
 
 public class ListContactsBean {
-    private ListContactsUseCase useCase = new ListContactsUseCase();
+
+    private ListContactsUseCase listUseCase = new ListContactsUseCase();
+    private DeleteContactUseCase deleteUseCase= new DeleteContactUseCase();
+
 
     @Getter @Setter
     private List<Contact> allContacts;
 
     @PostConstruct
     public void iniciate(){
-        this.allContacts= useCase.execute();
+        this.allContacts= listUseCase.execute();
+    }
+
+    public void delete(Integer id){
+        deleteUseCase.execute(id);
+        iniciate();
     }
 
 
